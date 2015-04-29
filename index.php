92d10eb0ae072ca96e4a7c00b1e3631fd2b3e907
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-04-29 11:52:14 -0700

    finished first two problems of the test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guillermo's Test</title>
+</head>
+<body>
+	<div class="Dan">
+		<button>Click Me</button>
+	</div>
+	<div class="Iwao">
+		
+	</div>
+	<?php
+		$( "p" ).on( "click",function() {
+			console.log("click")
+		});
+	?>
+</body>
+</html>
